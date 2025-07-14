a3d208d125f1da3ee7d86c44461892bfa2685dda
cesargomezs <32302962+cesargomezs@users.noreply.github.com>
2025-07-14 13:44:51 -0700

    Update home.tsx

--- src/components/sections/home.tsx
+++ src/components/sections/home.tsx
@@ -13,7 +13,7 @@ const Home = () => {
       Developed a prototype for a location-based legal services app that helps users find nearby lawyers by practice area. The system includes search by geolocation, interactive maps, and lawyer profile listings with contact options.  <br></br>
       <br></br>
       <strong>Lawyers Prototype:</strong>
-      <a href="https://cesargomezs.github.io/SampleLawyers/" target="_blank" className="myButton">Here</a>
+      <a href="https://cesargomezs.github.io/SampleLawyers/ " target="_blank" className="myButton">Here</a>
       </p>
     </section>
   );
